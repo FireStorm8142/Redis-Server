628786fdacdd3e693537f824f87f36594b72aa81
Johan Paul <johanjohn405@gmail.com>
2026-04-15 17:29:27 +0530

    Implemented Key TTL & Expiration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		System.out.println("Logs from your program will appear here!");
 		int port = 6379;
 		HashMap<String, String> storage = new HashMap<>();
+		HashMap<String, Long> expiry = new HashMap<>();
 		try {
 			Selector selector = Selector.open();
 			ServerSocketChannel serverChannel = ServerSocketChannel.open();
@@ -31,9 +32,8 @@ public class Main {
 					if (key.isAcceptable()){
 						handleAccept(serverChannel, selector);
 					} else if(key.isReadable()){
-						handleRead(key, storage);
+						handleRead(key, storage, expiry);
 					}
-					
 				}
 			}
 		} catch (IOException e) {
@@ -47,19 +47,20 @@ public class Main {
 		clientChannel.register(selector, SelectionKey.OP_READ);
 	}
 
-	private static void handleRead(SelectionKey key, HashMap<String, String> storage) throws IOException{
+	private static void handleRead(SelectionKey key, HashMap<String, String> storage, HashMap<String, Long> expiry) throws IOException{
 		SocketChannel clientChannel = (SocketChannel) key.channel();
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
         int bytesRead = clientChannel.read(buffer);
 
 		if (bytesRead == -1){
 			clientChannel.close();
+			return;
 		}
 
 		buffer.flip();
 		List<String> command = RespParser.parse(buffer);
-		
-		String cmd = command.get(0);
+
+		String cmd = command.get(0).toUpperCase();
 		String response;
 		switch(cmd){
 			case "ECHO":
@@ -74,6 +75,11 @@ public class Main {
 				break;
 
 			case "GET":
+				long now = System.currentTimeMillis();
+				if (expiry.getOrDefault(command.get(1), Long.MAX_VALUE) < now){
+						expiry.remove(command.get(1));
+						storage.remove(command.get(1));
+				}
 				String value = storage.getOrDefault(command.get(1), null);
 				String resp;
 				if (value != null) {
@@ -85,6 +91,14 @@ public class Main {
 				break;
 
 			case "SET":
+				if (command.size() > 3){
+					if (command.get(3).equalsIgnoreCase("PX")) {
+						expiry.put(command.get(1), System.currentTimeMillis() + Long.parseLong(command.get(4)));
+					}
+					else{
+						expiry.put(command.get(1), System.currentTimeMillis() + Long.parseLong(command.get(4))*1000);
+					}
+				}
 				storage.put(command.get(1), command.get(2));
 				clientChannel.write(ByteBuffer.wrap(("+OK\r\n").getBytes()));
 				break;
